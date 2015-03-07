3f7246fa38b63f36758f81773607f36604062c9d
Wolfy-J <wolfy.jd@gmail.com>
2015-03-07 19:54:36 +0300

    Legacy code removal.

--- application/config/views.php
+++ application/config/views.php
@@ -10,7 +10,6 @@ return array(
         'spiral'  => array(
             directory("application") . '/views/spiral/',
             directory("framework") . '/views/',
-            directory("application") . '/classes/Spiral/Markdown/views/',
             directory("libraries") . '/spiral/markdown/views/'
         )
     ),
