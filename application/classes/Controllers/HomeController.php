3a2c9b37bd9ec03a2424ed5464328675b02dd5fc
Wolfy-J <wolfy.jd@gmail.com>
2015-03-12 00:47:19 +0300

    Code ajusted  to latest updates.

--- application/classes/Controllers/HomeController.php
+++ application/classes/Controllers/HomeController.php
@@ -14,6 +14,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        echo $this->view->render('home');
+        return $this->view->render('home');
     }
 }
---
+++ application/config/http.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+return array();
