52f1f5965992c1fc8329b5f91cd7fa1852981dfa
Wolfy-J <wolfy.jd@gmail.com>
2015-03-14 23:26:04 +0300

    Http config updated.

--- application/config/http.php
+++ application/config/http.php
@@ -6,28 +6,39 @@
  * @author    Anton Titov (Wolfy-J)
  * @copyright ©2009-2015
  */
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
 return array(
-    'basePath'      => '/',
-    'showSnapshots' => true,
-    'cookies'       => array(
-        'subDomains' => true
+    'basePath'     => '/',
+    'exposeErrors' => true,
+    'cookies'      => array(
+        'subDomains' => false
     ),
-    'filters'       => array(
-        'Spiral\Components\Http\CsrfChecker',
+    'middlewares'  => array(
+        'Spiral\Profiler\Profiler',
+        'Spiral\Components\Http\CsrfToken',
         'Spiral\Components\Http\Cookies\CookieManager',
-        'Spiral\Profiler\Profiler'
     ),
-    'endpoints'     => array(),
-    'routes'        => array(
-        'default' => array(
+    'endpoints'    => array(),
+    'router'       => array(
+        'class'       => 'Spiral\Components\Http\Router',
+        'default'     => array(
             'pattern' => '(<controller>(/<action>(/<id>)))',
             'target'  => 'Controllers\<controller>::<action>'
         ),
-        'filters' => array(
+        'middlewares' => array(
             'crsf' => 'Spiral\Components\Http\CsrfChecker'
         )
     ),
-    'httpErrors'    => array(
+    'headers'      => array(
+        'Content-Type' => 'text/html; charset=UTF-8'
+    ),
+    'httpErrors'   => array(
         400 => 'spiral:http/badRequest',
         403 => 'spiral:http/forbidden',
         404 => 'spiral:http/notFound',
