9444c1cf668b61b34eb7d10d06b522147516b928
Wolfy-J <wolfy.jd@gmail.com>
2015-03-17 00:48:30 +0300

    Config updated.

--- application/config/http.php
+++ application/config/http.php
@@ -14,32 +14,33 @@
  * @copyright ©2009-2015
  */
 return array(
-    'basePath'     => '/',
-    'exposeErrors' => true,
-    'cookies'      => array(
+    'basePath'         => '/',
+    'exposeErrors'     => true,
+    'cookies'          => array(
         'subDomains' => false
     ),
-    'middlewares'  => array(
-        'Spiral\Profiler\Profiler',
-        'Spiral\Components\Http\CsrfProtector',
-        'Spiral\Components\Session\Http\SessionStarter',
-        'Spiral\Components\Http\Cookies\CookieManager',
-    ),
-    'endpoints'    => array(),
-    'router'       => array(
-        'class'             => 'Spiral\Components\Http\Router\Router',
-        'default'           => array(
-            'pattern' => '(<controller>(/<action>(/<id>)))',
-            'target'  => 'Controllers\<controller>::<action>'
-        ),
-        'middlewareAliases' => array(
-            'crsf' => 'Spiral\Components\Http\CsrfProtector'
-        )
-    ),
-    'headers'      => array(
+    'headers'          => array(
         'Content-Type' => 'text/html; charset=UTF-8'
     ),
-    'httpErrors'   => array(
+    'middlewares'      => array(
+        'Spiral\Components\Http\Middlewares\CsrfToken',
+        'Spiral\Components\Session\Http\SessionStarter',
+        'Spiral\Components\Http\Cookies\CookieManager',
+        //'Spiral\Profiler\Profiler'
+    ),
+    'endpoints'        => array(),
+    'router'           => array(
+        'class'        => 'Spiral\Components\Http\Router\Router',
+        'defaultRoute' => array(
+            'pattern'  => '(<controller>(/<action>(/<id>)))',
+            'target'   => 'Controllers\<controller>Controller::<action>',
+            'defaults' => array(
+                'controller' => 'home'
+            )
+        )
+    ),
+    'routeMiddlewares' => array(),
+    'httpErrors'       => array(
         400 => 'spiral:http/badRequest',
         403 => 'spiral:http/forbidden',
         404 => 'spiral:http/notFound',
