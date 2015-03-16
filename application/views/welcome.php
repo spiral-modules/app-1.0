c25132cb20d6066a9ddf7fd518cc1221c69bbfa0
Wolfy-J <wolfy.jd@gmail.com>
2015-03-16 03:59:58 +0300

    Welcome page updated.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,8 +3,19 @@
 
 <block:content>
     <img src="@{basePath}resources/images/spiral.svg" width="120px"/>
+
     <spiral:markdown>
         Welcome
         =======
     </spiral:markdown>
+
+    <a href="https://github.com/spiral-php/application">GitHub</a>
+    | <a href="https://github.com/spiral-php/documentation">Documentation (unfinished)</a>
+
+    <br/><br/>
+
+    <span style="font-size: 12px;">
+        This view file located in /application/views/welcome.php and called by Controllers\HomeController.
+    </span>
+
 </block:content>
