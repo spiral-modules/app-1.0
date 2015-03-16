17323cd8342a2d46254d0c68c7e1c7d10ccbc201
Wolfy-J <wolfy.jd@gmail.com>
2015-03-17 01:10:19 +0300

    Examples.

--- application/classes/Controllers/HomeController.php
+++ application/classes/Controllers/HomeController.php
@@ -8,12 +8,56 @@
  */
 namespace Controllers;
 
+use Spiral\Components\Http\Response\RedirectResponse;
 use Spiral\Core\Controller;
 
 class HomeController extends Controller
 {
+    /**
+     * Method available by /home/index or /home or /
+     *
+     * @return mixed
+     */
     public function index()
     {
         return $this->view->render('welcome');
+    }
+
+    /**
+     * Method available by /home/json
+     *
+     * @return array
+     */
+    public function json()
+    {
+        return array(
+            'status' => 200,
+            'data'   => [
+                1, 2, 3, 4, 5, 6, 7,
+            ]
+        );
+    }
+
+    /**
+     * Method available by /home/redirect
+     *
+     * @return RedirectResponse
+     */
+    public function redirect()
+    {
+        return new RedirectResponse('http://google.com/');
+    }
+
+    /**
+     * Method available by /home/internalRedirect
+     *
+     * @return RedirectResponse
+     */
+    public function internalRedirect()
+    {
+        return $this->router->redirect('default', [
+            'controller' => 'home',
+            'action'     => 'json'
+        ]);
     }
 }
