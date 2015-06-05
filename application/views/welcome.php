b5eaf8dd9c9c2a5488b216ce019479545cfc8828
Wolfy-J <wolfy.jd@gmail.com>
2015-06-05 20:17:20 +0300

    welcome view updated.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -7,13 +7,12 @@
         #Welcome To Spiral
     </spiral:markdown>
 
-    <a href="https://github.com/spiral-php/application">GitHub</a>
-    |
+    <a href="${git|https://github.com/spiral-php/application}">GitHub</a> |
     <a href="https://github.com/spiral-php/documentation">Documentation (unfinished)</a>
 
     <div style="font-size: 12px; margin-top: 10px;">
         This view file is located in
-        <a href="https://github.com/spiral-php/application/blob/master/application/views/welcome.php">application/views/welcome.php</a>
+        <a href="${git}/blob/master/application/views/welcome.php">application/views/welcome.php</a>
         and rendered by <b>Controllers\HomeController</b>.
     </div>
 </block:content>
