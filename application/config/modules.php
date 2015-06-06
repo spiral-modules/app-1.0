dfec9906aebbf9548037aae68b7a771fd7d434c8
Wolfy-J <wolfy.jd@gmail.com>
2015-06-06 20:38:14 +0300

    modules file added.

---
+++ application/config/modules.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'spiral/profiler' => array(
+        'class'     => 'Spiral\\Profiler\\Profiler',
+        'bootstrap' => false,
+        'bindings'  => array()
+    ),
+    'spiral/toolkit'  => array(
+        'class'     => 'Spiral\\Toolkit\\Toolkit',
+        'bootstrap' => false,
+        'bindings'  => array()
+    )
+);
