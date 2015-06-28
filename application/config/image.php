c378271a8391bb7ba14c2dd3b69b6f9b1b80e85c
Wolfy-J <wolfy.jd@gmail.com>
2015-06-28 17:46:37 +0300

    Default image module added.

---
+++ application/config/image.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Image configuration defines what driver to be used for image processing.
+ */
+return array(
+    'driver' => 'imagick'
+);
--- application/config/modules.php
+++ application/config/modules.php
@@ -1,12 +1,17 @@
 <?php
 return array(
-    'spiral/profiler' => array(
+    'spiral/profiler'           => array(
         'class'     => 'Spiral\\Profiler\\Profiler',
         'bootstrap' => false,
         'bindings'  => array()
     ),
-    'spiral/toolkit'  => array(
+    'spiral/toolkit'            => array(
         'class'     => 'Spiral\\Toolkit\\Toolkit',
+        'bootstrap' => false,
+        'bindings'  => array()
+    ),
+    'spiral/intervention-image' => array(
+        'class'     => 'Spiral\\Components\\Image\\ImageManager',
         'bootstrap' => false,
         'bindings'  => array()
     )
