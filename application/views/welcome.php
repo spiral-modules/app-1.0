38e8cce13c83cd08703dfe343a883360e081a995
Wolfy-J <wolfy.jd@gmail.com>
2015-07-25 01:20:04 +0300

    View updated.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,16 +1,16 @@
-<extends:layouts.simple title="[[Welcome to Spiral Framework!]]"/>
+<extends:layouts.simple title="[[Welcome to Spiral Framework!]]" git="https://github.com/spiral-php"/>
 
 <block:content>
     <img src="@{basePath}resources/images/spiral.svg" width="120px"/>
 
     <spiral:markdown>#Welcome To Spiral</spiral:markdown>
 
-    <a href="${git|https://github.com/spiral-php/application}">GitHub</a> |
-    <a href="https://github.com/spiral-php/documentation">Documentation (unfinished)</a>
+    <a href="${git}/application">GitHub</a> |
+    <a href="${git}/documentation">Documentation (unfinished)</a>
 
     <div style="font-size: 12px; margin-top: 10px;">
         This view file is located in
-        <a href="${git}/blob/master/application/views/welcome.php">application/views/welcome.php</a>
+        <a href="${git}/application/blob/master/application/views/welcome.php">application/views/welcome.php</a>
         and rendered by <b>Controllers\HomeController</b>.
     </div>
 </block:content>
