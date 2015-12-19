fc27391599c9f2fa02eed75aeff954cf41ec94a6
Wolfy-J <wolfy.jd@gmail.com>
2015-12-19 18:14:45 +0300

    Sample tests

--- tests/Cases/HomeTest.php
+++ tests/Cases/HomeTest.php
@@ -18,7 +18,19 @@ class HomeTest extends TestCase
     {
         $home = $this->get('/index.html');
 
+        //Sample middleware
         $this->assertArrayHasKey('My-Header', $home->getHeaders());
         $this->assertContains('Welcome to Spiral Framework', (string)$home->getBody());
+    }
+
+    public function testHomepageRu()
+    {
+        $home = $this->get('/index.html', [], [], [
+            'Accept-Language' => 'ru_RU,ru,en'
+        ]);
+
+        //Sample middleware
+        $this->assertArrayHasKey('My-Header', $home->getHeaders());
+        $this->assertContains('Добро пожаловать в Spiral Framework', (string)$home->getBody());
     }
 }
