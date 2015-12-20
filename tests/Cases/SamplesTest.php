034f6c79baba6c21593c8fd210a7688577d1e1a8
Wolfy-J <wolfy.jd@gmail.com>
2015-12-20 21:42:40 +0300

    few tests

---
+++ tests/Cases/SamplesTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+namespace Tests\Cases;
+
+use Tests\TestCase;
+use Tests\Traits\HttpTrait;
+
+class SamplesTest extends TestCase
+{
+    use HttpTrait;
+
+    public function testList()
+    {
+        $response = $this->get('/sample');
+
+        $this->assertContains('Edit element', (string)$response->getBody());
+    }
+
+    public function testElement()
+    {
+        $response = $this->get('/sample/edit/1');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testBadElement()
+    {
+        $response = $this->get('/sample/edit/abc');
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
