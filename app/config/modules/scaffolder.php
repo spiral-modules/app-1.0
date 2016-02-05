d208be3afffe9befcc774b07a6467762825a25c9
Wolfy-J <wolfy.jd@gmail.com>
2016-02-05 21:57:22 +0300

    updated scaffolder config

--- app/config/modules/scaffolder.php
+++ app/config/modules/scaffolder.php
@@ -33,27 +33,92 @@ return [
      * This is set of default settings to be used for your scaffolding commands.
      */
     'elements'  => [
-        'controller' => [
+        'controller'     => [
             'namespace' => 'Controllers',
             'postfix'   => 'Controller',
             'class'     => Declarations\ControllerDeclaration::class
         ],
-        'middleware' => [
+        'middleware'     => [
             'namespace' => 'Middlewares',
             'postfix'   => '',
             'class'     => Declarations\MiddlewareDeclaration::class
         ],
-        'command'    => [
+        'command'        => [
             'namespace' => 'Commands',
             'postfix'   => 'Command',
             'class'     => Declarations\CommandDeclaration::class
         ],
-        'service'    => [
+        'service'        => [
             'namespace' => 'Models',
             'postfix'   => 'Service',
             'class'     => Declarations\ServiceDeclaration::class
         ],
-        'record'     => [
+        'request'        => [
+            'namespace' => 'Requests',
+            'postfix'   => '',
+            'class'     => Declarations\RequestDeclaration::class,
+            'mapping'   => [
+                'int'    => [
+                    'source'    => 'data',
+                    'setter'    => 'intval',
+                    'validates' => [
+                        'notEmpty',
+                        'integer'
+                    ]
+                ],
+                'float'  => [
+                    'source'    => 'data',
+                    'setter'    => 'floatval',
+                    'validates' => [
+                        'notEmpty',
+                        'float'
+                    ]
+                ],
+                'string' => [
+                    'source'    => 'data',
+                    'setter'    => 'strval',
+                    'validates' => [
+                        'notEmpty',
+                        'string'
+                    ]
+                ],
+                'bool'   => [
+                    'source'    => 'data',
+                    'setter'    => 'boolval',
+                    'validates' => [
+                        'notEmpty',
+                        'boolean'
+                    ]
+                ],
+                'email'  => [
+                    'source'    => 'data',
+                    'type'      => 'string',
+                    'setter'    => 'strval',
+                    'validates' => [
+                        'notEmpty',
+                        'string',
+                        'email'
+                    ]
+                ],
+                'file'   => [
+                    'source'    => 'file',
+                    'type'      => '\Psr\Http\Message\UploadedFileInterface',
+                    'validates' => [
+                        'file::uploaded'
+                    ]
+                ],
+                'image'  => [
+                    'source'    => 'file',
+                    'type'      => '\Psr\Http\Message\UploadedFileInterface',
+                    'validates' => [
+                        "image::uploaded",
+                        "image::valid"
+                    ]
+                ],
+                /*{{request.mapping}}*/
+            ]
+        ],
+        'record'         => [
             'namespace' => 'Database',
             'postfix'   => '',
             'class'     => Declarations\Database\RecordDeclaration::class
@@ -69,5 +134,5 @@ return [
             'class'     => Declarations\Database\DocumentEntityDeclaration::class
         ],
         /*{{generators}}*/
-    ]
+    ],
 ];
