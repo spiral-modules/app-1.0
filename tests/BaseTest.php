310ce595d86c58a3becb5969f4c065ca9045c5c2
Wolfy-J <wolfy.jd@gmail.com>
2017-03-22 12:39:18 +0300

    fix runtime path in tests

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -24,6 +24,7 @@ abstract class BaseTest extends TestCase
         $this->app = \App::init([
             'root'        => $root,
             'libraries'   => $root . 'vendor/',
+            'runtime'     => $root . 'runtime/',
             'application' => $root . 'app/',
         ], null, null, false);
 
